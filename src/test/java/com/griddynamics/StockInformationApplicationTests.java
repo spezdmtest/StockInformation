194ecee965cc8c193556977bc64f2a44919c23d0
spezdmtest <spezd.m.@gmail.com>
2023-09-08 17:42:22 +0300

    enable dependencies

---
+++ src/test/java/com/griddynamics/StockInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.griddynamics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
