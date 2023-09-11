587253d179444de45e49d78b1868cf7926046413
spezdmtest <spezd.m.@gmail.com>
2023-09-11 17:02:27 +0300

    Models and Configurations

--- src/main/java/com/griddynamics/entity/Company.java
+++ src/main/java/com/griddynamics/entity/Company.java
@@ -20,7 +20,7 @@ import java.io.Serializable;
 public class Company implements Persistable<Integer>, Serializable {
 
     @Serial
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     @Id
     private Integer id;
