2a3aaae01e9b91e02766a5d154fd97bc502571be
spezdmtest <spezd.m.@gmail.com>
2023-09-12 16:23:29 +0300

    Mapper

--- src/main/java/com/griddynamics/mapper/StockMapper.java
+++ src/main/java/com/griddynamics/mapper/StockMapper.java
@@ -7,9 +7,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StockMapper {
-
     StockDto mapToStock(Stock stock);
-
     @InheritInverseConfiguration
     Stock mapToStockDto(StockDto stockDto);
 }
