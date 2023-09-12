0c82d2492d520a37f7a6f5a1c1b8c69c7206b158
spezdmtest <spezd.m.@gmail.com>
2023-09-12 17:49:50 +0300

    Repository

--- src/main/java/com/griddynamics/repository/CustomRepository.java
+++ src/main/java/com/griddynamics/repository/CustomRepository.java
@@ -2,8 +2,10 @@ package com.griddynamics.repository;
 
 import com.griddynamics.entity.Company;
 import com.griddynamics.entity.Stock;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface CustomRepository {
     Mono<Company> save(Company company);
     Mono<Stock> saveStock(Stock stock);
