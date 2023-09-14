b180855f492cc01d3860f56547dff8648913a4db
spezdmtest <spezd.m.@gmail.com>
2023-09-14 12:16:03 +0300

    correct indentation

--- src/main/java/com/griddynamics/AsyncApiStockDataManagerMsApplication.java
+++ src/main/java/com/griddynamics/AsyncApiStockDataManagerMsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AsyncApiStockDataManagerMsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AsyncApiStockDataManagerMsApplication.class, args);
     }
