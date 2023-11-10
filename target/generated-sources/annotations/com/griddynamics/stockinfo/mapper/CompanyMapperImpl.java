408d86bf18b276fc6c982074ab5ab5994c0eaa2c
spezdmtest <spezd.m.@gmail.com>
2023-11-10 18:00:06 +0200

    CodeBuild

---
+++ target/generated-sources/annotations/com/griddynamics/stockinfo/mapper/CompanyMapperImpl.java
@@ -0,0 +1,48 @@
+package com.griddynamics.stockinfo.mapper;
+
+import com.griddynamics.stockinfo.dto.CompanyDto;
+import com.griddynamics.stockinfo.entity.Company;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-02T17:30:31+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.5 (Microsoft)"
+)
+@Component
+public class CompanyMapperImpl implements CompanyMapper {
+
+    @Override
+    public CompanyDto mapToCompany(Company company) {
+        if ( company == null ) {
+            return null;
+        }
+
+        Integer id = null;
+        String symbol = null;
+
+        id = company.getId();
+        symbol = company.getSymbol();
+
+        Boolean isEnabled = null;
+
+        CompanyDto companyDto = new CompanyDto( id, symbol, isEnabled );
+
+        return companyDto;
+    }
+
+    @Override
+    public Company mapToCompanyDto(CompanyDto companyDto) {
+        if ( companyDto == null ) {
+            return null;
+        }
+
+        Company.CompanyBuilder company = Company.builder();
+
+        company.id( companyDto.id() );
+        company.symbol( companyDto.symbol() );
+
+        return company.build();
+    }
+}
---
+++ target/generated-sources/annotations/com/griddynamics/stockinfo/mapper/StockMapperImpl.java
@@ -0,0 +1,62 @@
+package com.griddynamics.stockinfo.mapper;
+
+import com.griddynamics.stockinfo.dto.StockDto;
+import com.griddynamics.stockinfo.entity.Stock;
+import java.math.BigDecimal;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-10-02T17:30:31+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.5 (Microsoft)"
+)
+@Component
+public class StockMapperImpl implements StockMapper {
+
+    @Override
+    public StockDto mapToStock(Stock stock) {
+        if ( stock == null ) {
+            return null;
+        }
+
+        Long id = null;
+        String symbol = null;
+        BigDecimal latestPrice = null;
+        BigDecimal change = null;
+        Integer previousVolume = null;
+        Integer volume = null;
+        String companyName = null;
+
+        id = stock.getId();
+        symbol = stock.getSymbol();
+        latestPrice = stock.getLatestPrice();
+        change = stock.getChange();
+        previousVolume = stock.getPreviousVolume();
+        volume = stock.getVolume();
+        companyName = stock.getCompanyName();
+
+        StockDto stockDto = new StockDto( id, symbol, latestPrice, change, previousVolume, volume, companyName );
+
+        return stockDto;
+    }
+
+    @Override
+    public Stock mapToStockDto(StockDto stockDto) {
+        if ( stockDto == null ) {
+            return null;
+        }
+
+        Stock.StockBuilder stock = Stock.builder();
+
+        stock.id( stockDto.id() );
+        stock.symbol( stockDto.symbol() );
+        stock.latestPrice( stockDto.latestPrice() );
+        stock.change( stockDto.change() );
+        stock.previousVolume( stockDto.previousVolume() );
+        stock.volume( stockDto.volume() );
+        stock.companyName( stockDto.companyName() );
+
+        return stock.build();
+    }
+}
